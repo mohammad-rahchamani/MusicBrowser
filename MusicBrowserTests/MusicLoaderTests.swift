b99a4bc96caf920503c74f4e4b739393ccacbb6f
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-28 13:04:16 +0430

    music loader fails on invalid data and delivers valid data from api

--- MusicBrowserTests/MusicLoaderTests.swift
+++ MusicBrowserTests/MusicLoaderTests.swift
@@ -72,6 +72,15 @@ class URLProtocolStub: URLProtocol {
     
 }
 
+struct MusicAlbum: Equatable, Codable {
+    public let id: String
+    public let album: String
+    public let artist: String
+    public let cover: String
+    public let label: String
+    public let tracks: [String]
+    public let year: String
+}
 
 class MusicLoader {
     
@@ -83,10 +92,10 @@ class MusicLoader {
         self.url = url
     }
     
-    func load() -> AnyPublisher<Void, Error> {
+    func load() -> AnyPublisher<[MusicAlbum], Error> {
         session.dataTaskPublisher(for: self.url)
-            .map { _ in () }
-            .mapError { err in err as Error }
+            .map(\.data)
+            .decode(type: [MusicAlbum].self, decoder: JSONDecoder())
             .eraseToAnyPublisher()
     }
     
@@ -118,14 +127,52 @@ class MusicLoaderTests: XCTestCase {
     }
     
     func test_load_failsOnNetworkError() {
-        URLProtocolStub.stub(withData: nil, response: nil, error: NSError(domain: "", code: 0, userInfo: nil))
         let sut = makeSUT()
+        expectFailure(sut, stubbing: .init(data: nil,
+                                           response: nil,
+                                           error: anyNSError()))
+    }
+    
+    func test_load_failsOnInvalidData() {
+        let sut = makeSUT()
+        expectFailure(sut, stubbing: .init(data: Data(),
+                                           response: successResponse(),
+                                           error: nil))
+    }
+    
+    func test_load_deliversData() {
+        let sut = makeSUT()
+        let expectedAlbums = [anyAlbum(), anyAlbum(), anyAlbum()]
+        let encodedAlbums = data(from: expectedAlbums)
+        expectLoad(sut,
+                   result: expectedAlbums,
+                   stubbing: .init(data: encodedAlbums,
+                                   response: successResponse(),
+                                   error: nil))
+    }
+    
+    // MARK: - helpers
+    
+    func makeSUT(file: StaticString = #filePath,
+                 line: UInt = #line) -> MusicLoader {
+        let sut = MusicLoader(session: .shared, url: anyURL())
+        trackForMemoryLeak(sut, file: file, line: line)
+        return sut
+    }
+    
+    func expectFailure(_ sut: MusicLoader,
+                       stubbing stub: URLProtocolStub.Stub,
+                       file: StaticString = #filePath,
+                       line: UInt = #line) {
+        URLProtocolStub.stub(withData: stub.data,
+                             response: stub.response,
+                             error: stub.error)
         let exp = XCTestExpectation(description: "waiting for load")
         sut.load()
             .sink(receiveCompletion: { completion in
                 switch completion {
                 case .finished:
-                    XCTFail()
+                    XCTFail(file: file, line: line)
                 default:
                     ()
                 }
@@ -136,19 +183,63 @@ class MusicLoaderTests: XCTestCase {
         wait(for: [exp], timeout: 1)
     }
     
-    // MARK: - helpers
-    
-    func makeSUT(file: StaticString = #filePath,
-                 line: UInt = #line) -> MusicLoader {
-        let sut = MusicLoader(session: .shared, url: URL(string: "https://any-url.com")!)
-        trackForMemoryLeak(sut, file: file, line: line)
-        return sut
+    func expectLoad(_ sut: MusicLoader,
+                    result expectedResult: [MusicAlbum],
+                    stubbing stub: URLProtocolStub.Stub,
+                    file: StaticString = #filePath,
+                    line: UInt = #line) {
+        URLProtocolStub.stub(withData: stub.data,
+                             response: stub.response,
+                             error: stub.error)
+        let exp = XCTestExpectation(description: "waiting for load")
+        sut.load()
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure:
+                    XCTFail(file: file, line: line)
+                default:
+                    ()
+                }
+                exp.fulfill()
+            }, receiveValue: { result in
+                XCTAssertEqual(result, expectedResult)
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [exp], timeout: 1)
     }
     
-    func trackForMemoryLeak(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+    func trackForMemoryLeak(_ instance: AnyObject,
+                            file: StaticString = #filePath,
+                            line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "instance should be nil", file: file, line: line)
         }
+    }
+    
+    func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
+    }
+    
+    func successResponse() -> URLResponse {
+        HTTPURLResponse(url: anyURL(),
+                        statusCode: 200,
+                        httpVersion: nil,
+                        headerFields: nil)!
+    }
+    
+    func anyAlbum() -> MusicAlbum {
+        MusicAlbum(id: "id",
+                   album: "album",
+                   artist: "artist",
+                   cover: "cover",
+                   label: "label",
+                   tracks: ["track"],
+                   year: "year")
+    }
+    
+    func data(from albums: [MusicAlbum]) -> Data {
+        return try! JSONEncoder().encode(albums)
     }
     
     func anyNSError() -> NSError {
