181d1014be3e14f38abc41df57ee501483cacf0a
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-29 02:55:17 +0430

    add album list action and state projection for view model

--- MusicBrowser/View/AlbumListView.swift
+++ MusicBrowser/View/AlbumListView.swift
@@ -7,6 +7,40 @@
 
 import SwiftUI
 
+enum AlbumListViewModel {
+    
+    struct ViewState: Equatable {
+        var albums: [MusicAlbum]
+        var isLoading: Bool
+        
+        static var initialState: ViewState {
+            ViewState(albums: [], isLoading: false)
+        }
+        
+        static func fromAppState(_ state: AppState) -> ViewState {
+            switch state.albums {
+            case .loading:
+                return ViewState(albums: [], isLoading: true)
+            case .neverLoaded:
+                return ViewState(albums: [], isLoading: false)
+            case .loaded(let albums):
+                return ViewState(albums: albums, isLoading: false)
+                
+            }
+        }
+    }
+    
+    enum ViewAction: Equatable {
+        case show
+        
+        static func toAppAction(_ action: ViewAction) -> AppAction {
+            .load(.startLoading)
+        }
+        
+    }
+    
+}
+
 struct AlbumListView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
