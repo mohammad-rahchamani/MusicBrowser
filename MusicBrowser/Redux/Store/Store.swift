be7670328d13516ac788e7ae2f53fb880c91012a
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-29 02:48:17 +0430

    add store to compose app pipeline

---
+++ MusicBrowser/Redux/Store/Store.swift
@@ -0,0 +1,28 @@
+//
+//  Store.swift
+//  MusicBrowser
+//
+//  Created by Mohammad Rahchamani on 4/29/22.
+//
+
+import Foundation
+import SwiftRex
+import CombineRex
+
+class Store: ReduxStoreBase<AppAction, AppState> {
+    
+    static func createStore() -> Store {
+        
+        let appReducer = AlbumListReducer.lifted
+        
+        let albumLoader = MusicLoader(session: .shared,
+                                      url: URL(string: "https://1979673067.rsc.cdn77.org/music-albums.json")!)
+        
+        let appMiddleware = AlbumLoaderMiddleware(albumLoader: albumLoader.load()).lifted()
+        
+        return Store(subject: .combine(initialValue: .initialState),
+                     reducer: appReducer,
+                     middleware: appMiddleware)
+    }
+    
+}
