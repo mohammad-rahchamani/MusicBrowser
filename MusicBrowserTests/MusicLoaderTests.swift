183f28572c5b69c914a1e3b3ad4cb0d872fc83d6
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-28 12:23:13 +0430

    create test file

--- MusicBrowserTests/MusicLoaderTests.swift
+++ MusicBrowserTests/MusicLoaderTests.swift
@@ -1,14 +1,13 @@
 //
-//  MusicBrowserTests.swift
+//  MusicLoaderTests.swift
 //  MusicBrowserTests
 //
 //  Created by Mohammad Rahchamani on 4/28/22.
 //
 
 import XCTest
-@testable import MusicBrowser
 
-class MusicBrowserTests: XCTestCase {
+class MusicLoaderTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
