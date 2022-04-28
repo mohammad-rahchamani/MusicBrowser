a005d05b37ff0c8f33a6affe4aa6024f88bbeefc
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-28 11:19:12 +0430

    Initial Commit

---
+++ MusicBrowser/MusicBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBrowserApp.swift
+//  MusicBrowser
+//
+//  Created by Mohammad Rahchamani on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
