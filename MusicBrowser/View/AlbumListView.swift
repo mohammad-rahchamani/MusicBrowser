648a285f31e4936afc52c859821bf4696935b7d9
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-29 02:35:47 +0430

    create album list view

---
+++ MusicBrowser/View/AlbumListView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumListView.swift
+//  MusicBrowser
+//
+//  Created by Mohammad Rahchamani on 4/29/22.
+//
+
+import SwiftUI
+
+struct AlbumListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumListView()
+    }
+}
