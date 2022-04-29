b7da144ceb5437e2519d20f82d6068ba258d8fbc
Mohammad Rahchamani <mohammad.rahchamani@gmail.com>
2022-04-29 05:31:25 +0430

    add view model for dashboard

--- MusicBrowser/UI/Dashboard/DashboardView.swift
+++ MusicBrowser/UI/Dashboard/DashboardView.swift
@@ -6,9 +6,39 @@
 //
 
 import SwiftUI
+import CombineRex
 
 struct DashboardView: View {
+    
+    @ObservedObject var viewModel: ObservableViewModel<DashboardViewModel.ViewAction, DashboardViewModel.ViewState>
+    
+    let albumPage: () -> AnyView
+    let trackPage: () -> AnyView
+    
+    init(viewModel: ObservableViewModel<DashboardViewModel.ViewAction, DashboardViewModel.ViewState>,
+         albumPage: @escaping () -> AnyView,
+         trackPage: @escaping () -> AnyView) {
+        self.viewModel = viewModel
+        self.albumPage = albumPage
+        self.trackPage = trackPage
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        TabView(selection: $viewModel.state.selectedTab) {
+        TabView(selection: $viewModel.state.selectedTab) {
+            albumPage()
+                .tabItem({ Text ("Albums")})
+                .tag(TabState.album)
+            
+            trackPage()
+//            AlbumListView(viewModel: AlbumListViewModel.viewModel(from: store))
+                .tabItem({ Text ("Tracks")})
+                .tag(TabState.track)
+        }
+        .onChange(of: viewModel.state.selectedTab) { tab in
+            print("send select \(tab) action")
+            viewModel.dispatch(.select(tab))
+        }
     }
 }
+
---
+++ MusicBrowser/UI/Dashboard/DashboardViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  DashboardViewModel.swift
+//  MusicBrowser
+//
+//  Created by Mohammad Rahchamani on 4/29/22.
+//
+
+import Foundation
+import SwiftRex
+import CombineRex
+
+enum DashboardViewModel {
+    
+    static func viewModel<S: StoreType>(from store: S) -> ObservableViewModel<ViewAction, ViewState> where S.ActionType == AppAction, S.StateType == AppState {
+        store.projection(action: ViewAction.toAppAction(_:),
+                         state: ViewState.fromAppState(_:))
+            .asObservableViewModel(initialState: .initialState)
+    }
+    
+    struct ViewState: Equatable {
+        var selectedTab: TabState
+        
+        static var initialState: ViewState {
+            return ViewState(selectedTab: .album)
+        }
+        
+        static func fromAppState(_ state: AppState) -> ViewState {
+            ViewState(selectedTab: state.selectedTab)
+        }
+    }
+    
+    enum ViewAction: Equatable {
+        case select(TabState)
+        
+        static func toAppAction(_ action: ViewAction) -> AppAction {
+            switch action {
+            case .select(let tab):
+                return .tab(.select(tab))
+            }
+        }
+        
+    }
+    
+}
